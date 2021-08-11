092ec7493af79d165cd27c19e58ebac1c8f94596
macquarts <macquart.sherine@gmail.com>
2021-08-11 21:41:59 +0800

    updated About section

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -4,7 +4,17 @@ import TestimonialSection from "../components/testimonials";
 export default function AboutPage() {
     return (
         <>
-           Heroes 
+            We understand the need to be able to find experienced, reliable, caring certified individuals to look after your child/children when you are away from them.
+
+            Tiny Treasure care is a platform that lets you find carers for your children and carers to find you. Once you sign up with us, you will be able to immediately view a pool of active carers who are willing to help you care for your precious little ones.  Searching and Viewing Caregiver Profiles is free. You will need to subscribe to contact caregivers and read messages.
+
+            For Carers: Carers can apply for jobs by simply creating a profile in our application. Once you are signed in, you can apply for advertised jobs easily on our homepage. When you send a message to the parent, they will be notified of your message. If they wish to reply, they will contact you through the message centre and a notification will be mailed to you. 
+
+
+            We are adding a lot more features on our applications, in the meantime, let us build the village that can help parents raise their children.
+
+
+
         </>
     );
 
