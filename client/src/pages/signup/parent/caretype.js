af53e947f8e83e307e8d070cccd6cd0347d8cd3f
macquarts <macquart.sherine@gmail.com>
2021-08-11 18:58:51 +0800

    Changes made with carer criteria

--- client/src/pages/signup/parent/caretype.js
+++ client/src/pages/signup/parent/caretype.js
@@ -15,23 +15,27 @@ import {
   } from '@chakra-ui/react';
   import { CheckIcon } from '@chakra-ui/icons';
   import Card from '../../../components/card';
-  
+
   export default function CareType(props) {
     return (
       <>
+
+      {/*  First Point of search for parents */}
         <Container>
           <Heading size="lg" marginBottom="10">
-            What kind of care are you looking for?
+           Type of Care Giver:
           </Heading>
           <Grid templateColumns="repeat(3, 1fr)" gap={6}>
            
             {[
               {
-                value: 'Baby Sitters',
-                label: 'Baby Sitters',
+                value: 'Baby Sitter',
+                label: 'Baby Sitter',
               },
-              { value: 'Nannies', label: 'Nannies' },
+              { value: 'Nanny', label: 'Nanny' },
               { value: 'Au Pair', label: 'Au Pair' },
+              {value: 'All Caregiver Work', label: 'All Caregiver Work'},
+
             ].map(item => {
               return (
                 <Card
