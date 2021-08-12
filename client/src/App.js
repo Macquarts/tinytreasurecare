0cddba855bdb2e694c70b798c919c8d44679ba17
macquarts <macquart.sherine@gmail.com>
2021-08-12 12:50:38 +0800

    added utils updated app.js and added carersignup

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,34 @@ import Footer from './components/footer';
 import SignUpPage from './pages/signup/signup';
 import ParentSignup from './pages/signup/parent/parentSignup';
 import CarerSignup from './pages/signup/carer/carerSignup';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: 'https://url',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 
 
