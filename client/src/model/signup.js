1bf4543e77b6bf103b6e774084b4b0b321c8bce4
macquarts <macquart.sherine@gmail.com>
2021-08-12 10:50:36 +0800

    signup working tree updated

---
+++ client/src/model/signup.js
@@ -0,0 +1,15 @@
+const parentObject = {
+    careType: "CARER",
+    expectedTime: "now",
+    criteria: {
+        carerExperience: "3",
+        carerGoodwith: "twotofiveyears",
+        postcode: "6000"
+    },
+    firstName: "Harry",
+    lastName: "Potter",
+    email: "harrypotter@gmail.com",
+    password: "123456",
+    type: "PARENT"
+
+}
