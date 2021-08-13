2c157e88ec78a5585c4e9b7035c037bea0b8bc34
macquarts <macquart.sherine@gmail.com>
2021-08-13 11:27:34 +0800

    set up user and index at server

---
+++ server/server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./shared/auth");
+
+const expressServer = express();
+const PORT = process.env.PORT || 3001;
+
+mongodb+srv://dbUSER:ClUsTeR9@cluster0.vih1l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+apolloServer.applyMiddleware({ app: expressServer });
+
+expressServer.use(express.urlencoded({ extended: true }));
+expressServer.use(express.json());
+
+// To server everything as a single app we are using static file serving 
+if (process.env.NODE_ENV === "production") {
+  expressServer.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+// navigate here when user loads URL
+expressServer.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+db.once("open", () => {
+  expressServer.listen(PORT, () =>
+    console.log(`Now listening on localhost:${PORT}`)
+  );
+  console.log(
+    `Use GraphQL at http://localhost:${PORT}${apolloServer.graphqlPath}`
+  );
+});
+
+
+const express = require("express");
+const app = express();
+// This is a sample test API key. Sign in to see examples pre-filled with your key.
+const stripe = require("stripe")("sk_test_wsFx86XDJWwmE4dMskBgJYrt");
+
+app.use(express.static("."));
+app.use(express.json());
+
+const calculateOrderAmount = items => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400;
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "usd"
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  });
+});
+
+app.listen(4242, () => console.log('Node server listening on port 4242!'));
