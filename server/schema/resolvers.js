d1e969b23a200617bc345321593dad4e55ecafd8
macquarts <macquart.sherine@gmail.com>
2021-08-14 15:37:50 +0800

    added link from client to graphQL

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,54 +1,97 @@
-const {AuthenticationError} =require("apollo-server-express");
-const { User} =require('../models');
-const{signToken} =require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User, JobPost } = require("../models");
+const { signToken } = require("../shared/auth");
 
-const resolvers={
-    Query: {
-        me: async (parent, args, context) => {
-          console.log(context.user);
-          if (context.user) {
-            const userData = await User.findOne({ _id: context.user._id });
-            return userData;
-          }
-        },
-        getUsers: async (parent, args, context) => {
-          const userList = await User.find();
-          return userList;
-        },
-        getRequests: async (parent, args, context) => {
-          return [];
-        }
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      console.log(context.user);
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id });
+        return userData;
+      }
+    },
+    getUsers: async (parent, args, context) => {
+      const userList = await User.find();
+      return userList;
+    },
+    getCarerDetail: async (parent, args, context) => {
+      const carerDetail = await User.findById(args[0].id); 
+      return carerDetail;
+    },
+    getCarers: async (parent, args, context) => {
+      const carerList =await User.find();
+      return carerList;  
+    },
+    getSentRequests: async (parent, args, context) => {
+      console.log(context.user);
+      if(!context.user) {
+        throw new AuthenticationError("Something went wrong");
+      }
+
+      const sentRequests = await JobPost.find({parentId: context.user._id});
+      console.log(sentRequests);
+      return sentRequests;
+    },
+    getRecievedRequests: async (parent, args, context) => {
+      console.log(context.user);
+      if(!context.user) {
+        throw new AuthenticationError("Something went wrong");
+      }
+
+      const recievedRequests = await JobPost.find({carerId: context.user._id});
+      console.log(recievedRequests);
+      return recievedRequests;
+    }
+
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+
+      if (!user) {
+        throw new AuthenticationError("Something is wrong");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    loginUser: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Can't find this user");
+      }
+
+      const correctPassword = await user.isCorrectPassword(password);
+
+      if (!correctPassword) {
+        throw new AuthenticationError("Wrong password!");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    sendJobRequest: async (parent,{carerId}, context) => {
+      console.log("User context",context.user, carerId);
+      if(!context.user) {
+        throw new AuthenticationError("Something went wrong");
+      }
+      const jobPost = await JobPost.create({carerId: carerId, parentId: context.user._id, jobStatus: "PENDING"});
+      console.log(jobPost);
+
+      return jobPost;
+    },
+    updateJobRequest: async (parent, {jobId, jobStatus}) => {
+      console.log(jobId, jobStatus);
+      if(!context.user) {
+        throw new AuthenticationError("Something went wrong");
+      }
+      return {_id: jobId, carerId: context.user._id, parentId: "782368",  jobStatus}
+    }
+
     
-      },
-      Mutation: {
-        addUser: async (parent, { username, email, password }) => {
-          const user = await User.create({ username, email, password });
-    
-          if (!user) {
-            throw new AuthenticationError("Something is wrong");
-          }
-          const token = signToken(user);
-          return { token, user };
-        },
-        loginUser: async (parent, { email, password }) => {
-          const user = await User.findOne({ email });
-    
-          if (!user) {
-            throw new AuthenticationError("Can't find this user");
-          }
-    
-          const correctPassword = await user.isCorrectPassword(password);
-    
-          if (!correctPassword) {
-            throw new AuthenticationError("Wrong password!");
-          }
-    
-          const token = signToken(user);
-    
-          return { token, user };
-        },
-      },
-    };
-    
-    module.exports = resolvers;
-    
+  },
+};
+
+module.exports = resolvers;
